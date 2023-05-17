12f0e1c119e8295f0d2e8df12e6587365bd747e4
Yacin Tmimi <yacintmimi@gmail.com>
2023-05-16 23:28:58 -0400

    Break project into a main.rs and lib.rs
    Doing this helps us more easily organize our code. Our `main.rs` file is
    primarily used as the entry point into our application, and shouldn't
    contain a lot of complicated logic. Our `lib.rs` is the entry point into
    our project's library, and in larger projects you'll want to define the
    bulk of your logic there. Typically you'll break `lib.rs` into sub
    modules to further organize the code, but this project will be pretty
    simple that that would be a little overkill.

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use axum::response::Json;
+use axum::routing::get;
+use axum::Router;
+use serde::Serialize;
+
+/// Build our application with a router
+/// Note: `get_app` is marked as `pub` so that it's "public" and accessible from outside of our
+/// library. Typically you'll want to define code in a `library`. `lib.rs` is typically the entry
+/// point into your library, while `main.rs` is the entry point into your `compiled binary`.
+/// By defining most of our application logic in a library our `main.rs` file becomes relatively
+/// simple, and writing integration tests to test your libraries public interface becomes easier.
+pub fn get_app() -> Router {
+    Router::new().route("/", get(hello_world))
+}
+
+// So what is `#[derive(Debug, Serialize)]`?
+//
+// Both `Debug` and `Serialize` are traits. We didn't discuss traits in our talk,
+// but they're awsome! Traits are rust's version of interfaces in Golang. Traits
+// define a set of methods and types can implement a trait by implementing their methods.
+// What's nice is we don't always need to manual implement traits for our types.
+// `Debug` is a trait provided by the standard library and The compliler can generte
+// a `Debug` implementation for us. The serde crate conveniently provides a macro that
+// implement `Serialize` for us too!
+//
+// Just like that we're now able to debug print out `HelloWordMessage` struct, and we're
+// able to serialize the data in our struct.
+#[derive(Debug, Serialize)]
+struct HelloWorldMessage {
+    message: String,
+}
+
+/// Returns a Hello World message to the user!
+/// You can read more about handlers [here](https://docs.rs/axum/latest/axum/handler/index.html)
+async fn hello_world() -> Json<HelloWorldMessage> {
+    let message = "Hello World!";
+    Json(HelloWorldMessage {
+        message: message.to_owned(),
+    })
+}
