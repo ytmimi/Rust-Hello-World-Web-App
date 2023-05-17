9c4398bd1afd74bc46bcec8f675097f6e5e04339
Yacin Tmimi <yacintmimi@gmail.com>
2023-05-16 23:47:59 -0400

    Add a test to validate our `hello_world` route
    This test validates that calling the route returns a "Hello World!"
    message.

---
+++ tests/hello_world_tests.rs
@@ -0,0 +1,22 @@
+// use the get_app function we defined to get our router
+use hello_world_web_server::get_app;
+
+use axum::body::Body;
+use axum::http::{Request, StatusCode};
+
+// The ServiceExt needs to be in scope to use oneshot
+use tower::ServiceExt;
+
+#[tokio::test]
+async fn hello_world() {
+    let app = get_app();
+    let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+
+    // `Router` implements `tower::Service<Request<Body>>` so we can
+    // call it like any tower service, no need to run an HTTP server.
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    assert_eq!(&body[..], b"{\"message\":\"Hello World!\"}");
+}
